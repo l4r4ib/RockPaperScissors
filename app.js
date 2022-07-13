bf0d917b32274dd92162bf6fb2e6accb88553eb1
laraib shuja <105169013+l4r4ib@users.noreply.github.com>
2022-07-13 17:40:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,42 +1,32 @@
-
-let comInput = Math.floor(Math.random()*3) +1 
-
-// let comInput = Math.floor(Math.random()*3)+1
-// console.log(comInput);
+let comInput = Math.floor(Math.random() * 3) + 1;
 
 let userScore = 0;
 let comScore = 0;
 let cCounter = document.querySelector("#Ccounter");
 let pCounter = document.querySelector("#Pcounter");
 const result = document.querySelector("h2");
+const enemyResult = document.querySelector("#result");
+let enemyPick = "";
 
-
-// console.log(comInput)
 const win = (result) => {
   result.innerText = "YOU WIN!!! CONGRATULATIONS";
-  userScore += 1
-  pCounter.innerText= userScore
-  
-  
+  userScore += 1;
+  pCounter.innerText = userScore;
 };
 const lose = (result) => {
   result.innerText = "YOU LOSE!! BETTER LUCK NEXT TIME";
-  comScore += 1
-  cCounter.innerText= comScore
- 
-
+  comScore += 1;
+  cCounter.innerText = comScore;
 };
 const draw = (result) => {
   result.innerText = "THE MATCH WAS A DRAW";
-  
-
 };
 
 const userInput = document.querySelectorAll(".input").forEach((e) => {
   e.addEventListener("click", (input) => {
     switch (e.alt) {
       case "rock":
-        comInput = Math.floor(Math.random()*3) +1 
+        comInput = Math.floor(Math.random() * 3) + 1;
         if (comInput === 1) draw(result);
         else if (comInput === 2) lose(result);
         else win(result);
@@ -44,7 +34,7 @@ const userInput = document.querySelectorAll(".input").forEach((e) => {
         break;
 
       case "paper":
-        comInput = Math.floor(Math.random()*3) +1 
+        comInput = Math.floor(Math.random() * 3) + 1;
         if (comInput === 2) draw(result);
         else if (comInput === 3) lose(result);
         else win(result);
@@ -52,7 +42,7 @@ const userInput = document.querySelectorAll(".input").forEach((e) => {
         break;
 
       case "scissors":
-        comInput = Math.floor(Math.random()*3) +1 
+        comInput = Math.floor(Math.random() * 3) + 1;
         if (comInput === 3) draw(result);
         else if (comInput === 1) lose(result);
         else win(result);
@@ -60,9 +50,19 @@ const userInput = document.querySelectorAll(".input").forEach((e) => {
         break;
 
       default:
-        comInput = Math.floor(Math.random()*3) +1 
+        comInput = Math.floor(Math.random() * 3) + 1;
         result.innerText = "YOU SOMEHOW TRIGGERED THE DEFAULT STATEMENT";
         break;
     }
+
+    if (comInput === 1) {
+      enemyPick = "rock";
+    } else if (comInput === 2) {
+      enemyPick = "paper";
+    } else {
+      enemyPick = "scissors";
+    }
+
+    enemyResult.innerHTML = `Computer picked ${enemyPick}`;
   });
 });
